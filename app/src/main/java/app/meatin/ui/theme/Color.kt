e4890e280f977578428c5de61b8c80982a132549
Beomjun Kim <nutyworks@gmail.com>
2021-09-01 21:31:59 +0900

    feat(presentation): implement design system (#12)
    * feat(presentation): add color components
    
    * feat(presentation): add pretendard font family

--- app/src/main/java/app/meatin/ui/theme/Color.kt
+++ app/src/main/java/app/meatin/ui/theme/Color.kt
@@ -2,7 +2,18 @@ package app.meatin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkFlamingo = Color(0xFFF65252)
+val Flamingo = Color(0xFFFF7777)
+val LightFlamingo = Color(0xFFFFC9C9)
+
+val ErrorLightPink = Color(0xFFFFE9E9)
+val ErrorRed = Color(0xFFD7260D)
+
+val DisableLightGray = Color(0xFFF8F8F8)
+val DisableLightGray2 = Color(0xFFEEEEEE)
+
+val BackgroundFullBrightGray = Color(0xFFF9FAFC)
+
+val BorderGray = Color(0xFFCCCCCC)
+
+val BoxTextDarkGray = Color(0xFF6F6F6F)
--- app/src/main/java/app/meatin/ui/theme/Type.kt
+++ app/src/main/java/app/meatin/ui/theme/Type.kt
@@ -1,28 +1,49 @@
 package app.meatin.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import app.meatin.R
 
-// Set of Material typography styles to start with
+val PretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+)
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        color = Color.Black,
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    h2 = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = Color.Black.copy(alpha = 0.6f),
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        color = Color.Black,
+    ),
+    body1 = TextStyle(
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 16.sp,
+        color = Color.Black,
+    ),
+    body2 = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = Color.Black.copy(alpha = 0.4f),
+    ),
 )
