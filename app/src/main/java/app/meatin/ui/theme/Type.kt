181eb764856e9e11f6e7da68056d50e21c4f6e5e
Beomjun Kim <nutyworks@gmail.com>
2021-09-01 21:06:56 +0900

    style

--- app/src/main/java/app/meatin/ui/theme/Type.kt
+++ app/src/main/java/app/meatin/ui/theme/Type.kt
@@ -2,12 +2,10 @@ package app.meatin.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import app.meatin.R
 
