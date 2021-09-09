c76367e6eed1dc3118a934a88d6f75a2254f88f0
jhk2004 <water2jhk@gmail.com>
2021-09-09 22:24:29 +0900

    fix: revert MainActivity.kt

--- app/src/main/java/app/meatin/MainActivity.kt
+++ app/src/main/java/app/meatin/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             MeatInTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android!")
+                    Greeting("Android")
                 }
             }
         }
