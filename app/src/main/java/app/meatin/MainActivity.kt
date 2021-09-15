7cd12008b55537df6a741849874be678ec7a25a3
Jungwook <64252201+namjungwook8133@users.noreply.github.com>
2021-09-15 20:31:33 +0900

    style: kotlin-lint

--- app/src/main/java/app/meatin/MainActivity.kt
+++ app/src/main/java/app/meatin/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.PostCardPreview
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +17,7 @@ class MainActivity : ComponentActivity() {
             MeatInTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PostCardPreview()
+                    Greeting("Android")
                 }
             }
         }
