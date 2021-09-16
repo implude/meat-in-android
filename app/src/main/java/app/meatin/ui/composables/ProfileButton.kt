0bbfb4ad06a063fe1c2001f9127c9c99fc7472c0
jhk2004 <water2jhk@gmail.com>
2021-09-16 21:33:05 +0900

    style

--- app/src/main/java/app/meatin/ui/composables/ProfileButton.kt
+++ app/src/main/java/app/meatin/ui/composables/ProfileButton.kt
@@ -3,7 +3,7 @@ package app.meatin.ui.composables
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
