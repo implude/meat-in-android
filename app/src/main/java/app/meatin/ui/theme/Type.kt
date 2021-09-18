78dd95f6694f5bb35c15574fad3d8952f0db5e2c
Beomjun Kim <nutyworks@gmail.com>
2021-09-18 14:51:25 +0900

    fix(presentation): change typography

--- app/src/main/java/app/meatin/ui/theme/Type.kt
+++ app/src/main/java/app/meatin/ui/theme/Type.kt
@@ -9,41 +9,40 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import app.meatin.R
 
-val PretendardFontFamily = FontFamily(
-    Font(R.font.pretendard_bold, FontWeight.Bold),
-    Font(R.font.pretendard_medium, FontWeight.Medium),
-    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
-    Font(R.font.pretendard_regular, FontWeight.Normal),
+val NotoSansFontFamily = FontFamily(
+    Font(R.font.noto_sans_cjk_kr_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_cjk_kr_medium, FontWeight.Medium),
+    Font(R.font.noto_sans_cjk_kr_bold, FontWeight.Bold),
 )
 
-@Deprecated("This value will be removed. Use MeatInTypography instead.")
+@Deprecated("Use MeatInTypography instead.")
 val Typography = Typography(
     h1 = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         color = Color.Black,
     ),
     h2 = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         color = Color.Black.copy(alpha = 0.6f),
     ),
     body1 = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         color = Color.Black,
     ),
     body2 = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         color = Color.Black,
     ),
     caption = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         color = Color.Black.copy(alpha = 0.4f),
@@ -51,46 +50,62 @@ val Typography = Typography(
 )
 
 object MeatInTypography {
+    private const val AUTO_LINE_HEIGHT = 1.5
+
     val pageTitle = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
+        lineHeight = (20 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.01).sp,
         color = Color.Black,
     )
     val sectionHeader = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
+        lineHeight = (16 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.01).sp,
         color = Color.Black,
     )
     val subHeader = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
+        lineHeight = (14 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.01).sp,
         color = Color.Black.copy(alpha = 0.6f),
     )
     val bigDescription = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
+        lineHeight = (20 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.01).sp,
         color = Color.Black,
     )
     val regularImportant = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
+        lineHeight = (14 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.01).sp,
         color = Color.Black,
     )
     val regular = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
+        lineHeight = (14 * AUTO_LINE_HEIGHT).sp,
+        letterSpacing = (-0.03).sp,
         color = Color.Black,
     )
     val description = TextStyle(
-        fontFamily = PretendardFontFamily,
+        fontFamily = NotoSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
+        lineHeight = (14 * 1.35).sp,
+        letterSpacing = (-0.03).sp,
         color = Color.Black.copy(alpha = 0.4f),
     )
 }
