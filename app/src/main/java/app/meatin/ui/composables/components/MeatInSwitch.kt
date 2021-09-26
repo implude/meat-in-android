dc5d71cfe9aa94559695b19da935a9465e256412
Beomjun Kim <nutyworks@gmail.com>
2021-09-26 11:13:37 +0900

    feat(presentation): add custom switch component (#33)
    * feat(presentation): add custom switch component
    
    * refactor: remove material switch preview
    
    * refactor: correct package location
    
    * refactor: update `MeatInSwitch` previews

---
+++ app/src/main/java/app/meatin/ui/composables/components/MeatInSwitch.kt
@@ -0,0 +1,195 @@
+package app.meatin.ui.composables.components
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.SwipeableDefaults
+import androidx.compose.material.SwipeableState
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material.swipeable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import app.meatin.ui.theme.DarkFlamingo
+import kotlin.math.roundToInt
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun MeatInSwitch(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    val minBound = 0f
+    val maxBound = with(LocalDensity.current) { ThumbPathLength.toPx() }
+    val swipeableState = rememberSwipeableStateFor(checked, onCheckedChange ?: {}, AnimationSpec)
+    val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
+    val toggleableModifier =
+        if (onCheckedChange != null) {
+            Modifier.toggleable(
+                value = checked,
+                onValueChange = onCheckedChange,
+                enabled = true,
+                role = Role.Switch,
+                interactionSource = interactionSource,
+                indication = null
+            )
+        } else {
+            Modifier
+        }
+
+    Box(
+        modifier
+            .then(toggleableModifier)
+            .swipeable(
+                state = swipeableState,
+                anchors = mapOf(minBound to false, maxBound to true),
+                thresholds = { _, _ -> FractionalThreshold(0.5f) },
+                orientation = Orientation.Horizontal,
+                enabled = onCheckedChange != null,
+                reverseDirection = isRtl,
+                interactionSource = interactionSource,
+                resistance = null
+            )
+            .wrapContentSize(Alignment.Center)
+            .padding(DefaultSwitchPadding)
+            .requiredSize(SwitchWidth, SwitchHeight)
+    ) {
+        SwitchImpl(
+            checked = checked,
+            thumbValue = swipeableState.offset,
+            interactionSource = interactionSource
+        )
+    }
+}
+
+@Composable
+private fun BoxScope.SwitchImpl(
+    checked: Boolean,
+    thumbValue: State<Float>,
+    interactionSource: MutableInteractionSource,
+) {
+    Canvas(
+        Modifier
+            .align(Alignment.Center)
+            .fillMaxSize()
+    ) {
+        drawTrack(Color(0xFFE1E1E1), TrackWidth.toPx(), TrackStrokeWidth.toPx())
+    }
+
+    Spacer(
+        Modifier
+            .align(Alignment.CenterStart)
+            .offset { IntOffset(thumbValue.value.roundToInt(), 0) }
+            .indication(
+                interactionSource = interactionSource,
+                indication = rememberRipple(bounded = false, radius = RippleRadius)
+            )
+            .requiredSize(ThumbDiameter)
+            .background(if (checked) DarkFlamingo else Color(0xFFF0F0F0), CircleShape)
+
+    )
+}
+
+private fun DrawScope.drawTrack(trackColor: Color, trackWidth: Float, strokeWidth: Float) {
+    val strokeRadius = strokeWidth / 2
+    drawLine(
+        trackColor,
+        Offset(strokeRadius, center.y),
+        Offset(trackWidth - strokeRadius, center.y),
+        strokeWidth,
+        StrokeCap.Round
+    )
+}
+
+private val TrackWidth = 36.dp
+private val TrackStrokeWidth = 4.dp
+private val DefaultSwitchPadding = 2.dp
+private val RippleRadius = 24.dp
+private val ThumbDiameter = 18.dp
+private val ThumbPathLength = TrackWidth - ThumbDiameter
+private val SwitchWidth = 36.dp
+private val SwitchHeight = 18.dp
+private val AnimationSpec = TweenSpec<Float>(durationMillis = 100)
+
+@Composable
+@ExperimentalMaterialApi
+internal fun <T : Any> rememberSwipeableStateFor(
+    value: T,
+    onValueChange: (T) -> Unit,
+    animationSpec: AnimationSpec<Float> = SwipeableDefaults.AnimationSpec,
+): SwipeableState<T> {
+    val swipeableState = remember {
+        SwipeableState(
+            initialValue = value,
+            animationSpec = animationSpec,
+            confirmStateChange = { true }
+        )
+    }
+    val forceAnimationCheck = remember { mutableStateOf(false) }
+    LaunchedEffect(value, forceAnimationCheck.value) {
+        if (value != swipeableState.currentValue) {
+            swipeableState.animateTo(value)
+        }
+    }
+    DisposableEffect(swipeableState.currentValue) {
+        if (value != swipeableState.currentValue) {
+            onValueChange(swipeableState.currentValue)
+            forceAnimationCheck.value = !forceAnimationCheck.value
+        }
+        onDispose { }
+    }
+    return swipeableState
+}
+
+@Preview(name = "Switch On")
+@Composable
+fun ToggleSwitchPreview1() {
+    MeatInSwitch(checked = true, onCheckedChange = {})
+}
+
+@Preview(name = "Switch Off")
+@Composable
+fun ToggleSwitchPreview2() {
+    MeatInSwitch(checked = false, onCheckedChange = {})
+}
+
+@Preview(name = "Animated Switch Preview", showBackground = true)
+@Composable
+fun AnimatedSwitchPreview() {
+    val (checked, setChecked) = remember { mutableStateOf(false) }
+    MeatInSwitch(checked = checked, onCheckedChange = setChecked, modifier = Modifier.padding(20.dp))
+}
