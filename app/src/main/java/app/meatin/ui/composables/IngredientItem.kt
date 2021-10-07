c1a1bd87289b581ff169fd7353b060dd54bab680
Jungwook <64252201+namjungwook8133@users.noreply.github.com>
2021-10-07 22:01:05 +0900

    ktlint

--- app/src/main/java/app/meatin/ui/composables/IngredientItem.kt
+++ app/src/main/java/app/meatin/ui/composables/IngredientItem.kt
@@ -3,23 +3,15 @@ package app.meatin.ui.composables
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +20,6 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import app.meatin.ui.theme.CoreText
 import app.meatin.ui.theme.DarkFlamingo
 import app.meatin.ui.theme.MeatInTypography
-import kotlinx.coroutines.NonDisposableHandle.parent
 
 @Composable
 fun IngredientItem(
