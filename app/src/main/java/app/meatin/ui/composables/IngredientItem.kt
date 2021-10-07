8d9f3723175b5c3f57533626053d25b5959c4021
Jungwook <64252201+namjungwook8133@users.noreply.github.com>
2021-10-07 22:43:04 +0900

    ktlint

--- app/src/main/java/app/meatin/ui/composables/IngredientItem.kt
+++ app/src/main/java/app/meatin/ui/composables/IngredientItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
