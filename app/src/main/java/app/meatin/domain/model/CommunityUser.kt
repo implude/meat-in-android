477fb150f3d5fa00ec745d8f257af16238226a23
Jungwook <64252201+namjungwook8133@users.noreply.github.com>
2021-10-13 20:35:05 +0900

    fix: BreifCommunityUser and CommunityUser are switched

--- app/src/main/java/app/meatin/domain/model/CommunityUser.kt
+++ app/src/main/java/app/meatin/domain/model/CommunityUser.kt
@@ -6,4 +6,7 @@ data class CommunityUser(
     val name: String,
     val profileImage: String,
     @SerializedName("rep_badge") val repBadge: BriefBadge,
+    @SerializedName("badge_list") val badges: List<Badge>,
+    @SerializedName("uploaded_post") val uploadedPosts: List<BriefPost>,
+    @SerializedName("uploaded_recipe") val uploadedRecipes: List<BriefRecipe>,
 )
