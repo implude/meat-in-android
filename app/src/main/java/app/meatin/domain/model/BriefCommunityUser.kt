e55434371b37c57c26923d79823965b80359dcb2
Alex Nam <64252201+namjungwook8133@users.noreply.github.com>
2021-10-13 20:53:23 +0900

    fix(domain): switch `BreifCommunityUser` and `CommunityUser` (#62)

--- app/src/main/java/app/meatin/domain/model/BriefCommunityUser.kt
+++ app/src/main/java/app/meatin/domain/model/BriefCommunityUser.kt
@@ -6,7 +6,4 @@ data class BriefCommunityUser(
     val name: String,
     val profileImage: String,
     @SerializedName("rep_badge") val repBadge: BriefBadge,
-    @SerializedName("badge_list") val badges: List<Badge>,
-    @SerializedName("uploaded_post") val uploadedPosts: List<BriefPost>,
-    @SerializedName("uploaded_recipe") val uploadedRecipes: List<BriefRecipe>,
 )
