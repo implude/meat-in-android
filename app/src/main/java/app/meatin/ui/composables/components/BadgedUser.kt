04e7ecedd36a492737f17786461063bddd885261
Beomjun Kim <nutyworks@gmail.com>
2021-10-14 20:22:42 +0900

    fix(presentation): import padding modifier

--- app/src/main/java/app/meatin/ui/composables/components/BadgedUser.kt
+++ app/src/main/java/app/meatin/ui/composables/components/BadgedUser.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
