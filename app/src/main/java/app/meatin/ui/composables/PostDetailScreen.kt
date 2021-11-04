6116f300c2f940200c3b376142707dc71665a7c4
gijuno <itx.ljh.developer@gmail.com>
2021-11-04 22:19:19 +0900

    feat: change requested changes

--- app/src/main/java/app/meatin/ui/composables/PostDetailScreen.kt
+++ app/src/main/java/app/meatin/ui/composables/PostDetailScreen.kt
@@ -3,7 +3,6 @@ package app.meatin.ui.composables
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,9 +22,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -37,33 +36,26 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import app.meatin.domain.model.Comment
 import app.meatin.domain.model.FakeValues
+import app.meatin.domain.model.Post
 import app.meatin.ui.composables.components.CommentItem
 import app.meatin.ui.composables.components.PostBottomAppBar
 import app.meatin.ui.composables.components.ProfileButton
 import app.meatin.ui.composables.components.TaggedRecipe
+import app.meatin.ui.theme.Flamingo
 import app.meatin.ui.theme.MeatInTypography
 import app.meatin.ui.theme.composefix.CoreText
+import app.meatin.util.defaultDateFormatter
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import java.net.URI
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @ExperimentalComposeUiApi
 @ExperimentalCoilApi
 @Composable
 fun PostDetailScreen(
     modifier: Modifier = Modifier,
-    titleImageURI: URI,
-    title: String,
-    date: LocalDateTime,
-    description: String,
-    comments: List<Comment>,
-    isHearted: Boolean,
-    isBookmarked: Boolean,
-    hearts: Int,
+    post: Post,
 ) {
     Scaffold(
         content = {
@@ -72,6 +64,7 @@ fun PostDetailScreen(
                     .background(color = Color(0xFFE5E5E5))
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
+                    .padding(bottom = 50.dp)
             ) {
                 ConstraintLayout(
                     modifier
@@ -96,7 +89,7 @@ fun PostDetailScreen(
                                 end.linkTo(parent.end)
                             },
                         painter = rememberImagePainter(
-                            data = titleImageURI.toASCIIString()
+                            data = URI(post.photos[0]).toASCIIString()
                         ),
                         contentDescription = null,
                         contentScale = ContentScale.Crop
@@ -108,7 +101,7 @@ fun PostDetailScreen(
                                 top.linkTo(titleImage.bottom, 16.dp)
                                 start.linkTo(parent.start, 16.dp)
                             },
-                        text = title,
+                        text = post.title,
                         style = MeatInTypography.sectionHeader,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1
@@ -120,7 +113,7 @@ fun PostDetailScreen(
                                 top.linkTo(titleText.bottom, 6.dp)
                                 start.linkTo(parent.start, 16.dp)
                             },
-                        text = DateTimeFormatter.ofPattern("yyyy. MM. dd").format(date),
+                        text = defaultDateFormatter.format(post.createdAt),
                         style = MeatInTypography.description,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1
@@ -134,7 +127,7 @@ fun PostDetailScreen(
                                 end.linkTo(parent.end, 16.dp)
                                 width = Dimension.fillToConstraints
                             }
-                            .shadow(4.dp),
+                            .shadow(1.dp),
                         user = FakeValues.BRIEF_COMMUNITY_USER,
                         profileUri = FakeValues.BRIEF_COMMUNITY_USER.profileImage,
                         onClick = { /*TODO*/ },
@@ -147,7 +140,7 @@ fun PostDetailScreen(
                                 start.linkTo(parent.start, 16.dp)
                             }
                             .padding(bottom = 16.dp),
-                        text = description,
+                        text = post.content,
                         style = MeatInTypography.regular,
                         overflow = TextOverflow.Clip
                     )
@@ -194,10 +187,12 @@ fun PostDetailScreen(
                     val (
                         commentText,
                         inputCommentTextField,
+                        postCommentButton,
                         commentItems,
                     ) = createRefs()
 
                     val relocationRequestor = remember { RelocationRequester() }
+                    val text = remember { mutableStateOf("") }
 
                     CoreText(
                         modifier = Modifier
@@ -208,7 +203,6 @@ fun PostDetailScreen(
                         text = "댓글",
                         style = MeatInTypography.sectionHeader
                     )
-
                     TextField(
                         modifier = Modifier
                             .constrainAs(inputCommentTextField) {
@@ -219,24 +213,33 @@ fun PostDetailScreen(
                             }
                             .requiredHeight(51.dp)
                             .relocationRequester(relocationRequestor),
-                        value = "",
+                        value = text.value,
                         onValueChange = {
                             relocationRequestor.bringIntoView()
+                            text.value = it
                         },
-                        shape = RoundedCornerShape(5.dp),
+                        shape = RoundedCornerShape(5.dp, 0.dp, 0.dp, 5.dp),
                         colors = TextFieldDefaults.textFieldColors(
                             backgroundColor = Color(0xfff8f8f8),
                             focusedIndicatorColor = Color.Transparent,
                             unfocusedIndicatorColor = Color.Transparent
                         ),
                         placeholder = {
-                            Box {
-                                CoreText(
-                                    text = "댓글을 입력해주세요",
-                                    style = MeatInTypography.regular,
-                                    modifier = Modifier.align(Alignment.CenterStart)
-                                )
-                            }
+                            CoreText(
+                                text = "댓글을 입력해주세요",
+                                style = MeatInTypography.regular.copy(color = Color(0xFF919191)),
+                            )
+                        },
+                        trailingIcon = {
+                            CoreText(
+                                modifier = modifier
+                                    .padding(start = 16.dp, end = 16.dp)
+                                    .background(Color(0xfff8f8f8))
+                                    .clip(RoundedCornerShape(0.dp, 5.dp, 5.dp, 0.dp)),
+                                text = "게시",
+                                color = Flamingo,
+                                style = MeatInTypography.subHeader,
+                            )
                         }
                     )
 
@@ -251,7 +254,7 @@ fun PostDetailScreen(
                             },
                         verticalArrangement = Arrangement.spacedBy(20.dp)
                     ) {
-                        for (comments in comments) {
+                        for (comments in post.comments) {
                             CommentItem(comment = comments)
                         }
                     }
@@ -264,12 +267,12 @@ fun PostDetailScreen(
 
             PostBottomAppBar(
                 isHearted = checkedHeart,
-                onToPostClick = {  },
+                onToPostClick = {},
                 onHeartClick = { checkedHeart = it },
                 onBookmarkClick = { checkedBookmark = it },
                 onShareClick = {},
                 isBookmarked = checkedBookmark,
-                hearts = hearts
+                hearts = post.heart.count
             )
         }
     )
@@ -281,13 +284,6 @@ fun PostDetailScreen(
 @Composable
 fun PostDetailScreenPreview() {
     PostDetailScreen(
-        titleImageURI = URI("https://naver.com"),
-        title = "육즙 가득 삼겹살 굽기!!",
-        date = LocalDateTime.of(2021, 9, 16, 1, 0, 0),
-        description = "고백받았는데 내가 거절했어\n한순간에 남같이 돌변하더라\n너무힘들어 지금도 울고있어\n보고싶다 매일밤 전화하던게\n넘너무 그리워 내 목숨을 가져가도 좋아\n제발 연락해줘",
-        comments = listOf(FakeValues.COMMENT, FakeValues.COMMENT, FakeValues.COMMENT),
-        isHearted = true,
-        isBookmarked = false,
-        hearts = 11
+        post = FakeValues.POST
     )
 }
