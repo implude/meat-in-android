da16237e6d3963122ccaa1a06e22a3aec07886b6
gijuno <itx.ljh.developer@gmail.com>
2021-11-11 20:24:07 +0900

    style: delete unused import

--- app/src/main/java/app/meatin/ui/composables/PostDetailScreen.kt
+++ app/src/main/java/app/meatin/ui/composables/PostDetailScreen.kt
@@ -3,7 +3,6 @@ package app.meatin.ui.composables
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -27,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
