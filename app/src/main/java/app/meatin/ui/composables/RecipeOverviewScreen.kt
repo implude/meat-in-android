cbd823e948ad3f946a2e0f8fca2bc21dfb314f29
Beomjun Kim <nutyworks@gmail.com>
2021-11-13 20:50:36 +0900

    refactor(presentation): refactor codes and improve security

--- app/src/main/java/app/meatin/ui/composables/RecipeOverviewScreen.kt
+++ app/src/main/java/app/meatin/ui/composables/RecipeOverviewScreen.kt
@@ -1,12 +1,10 @@
 package app.meatin.ui.composables
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -18,15 +16,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
@@ -35,7 +37,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import app.meatin.domain.model.BriefPost
 import app.meatin.domain.model.FakeValues
+import app.meatin.domain.model.Ingredient
 import app.meatin.domain.model.Recipe
 import app.meatin.ui.composables.components.IngredientItem
 import app.meatin.ui.composables.components.PostCard
@@ -49,8 +53,6 @@ import app.meatin.util.toDate
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 
-@SuppressLint("SetJavaScriptEnabled")
-@OptIn(ExperimentalFoundationApi::class, ExperimentalCoilApi::class)
 @Composable
 fun RecipeOverviewScreen(
     recipe: Recipe,
@@ -58,151 +60,14 @@ fun RecipeOverviewScreen(
 ) {
     LazyColumn(Modifier.background(DisableLightGray2)) {
         item {
-            Image(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp),
-                painter = rememberImagePainter(data = recipe.thumbnail),
-                contentDescription = "Recipe overview image",
-                contentScale = ContentScale.Crop,
-            )
-
-            ConstraintLayout(
-                Modifier
-                    .background(Color.White)
-                    .padding(16.dp)
-                    .fillMaxWidth()
-            ) {
-                val (
-                    titleText,
-                    dateText,
-                    shareIcon,
-                    descriptionText,
-                    profileButton,
-                    additionalInfoRow,
-                ) = createRefs()
-                CoreText(
-                    modifier = Modifier.constrainAs(titleText) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                    },
-                    text = recipe.name, style = MeatInTypography.sectionHeader
-                )
-                CoreText(
-                    modifier = Modifier.constrainAs(dateText) {
-                        bottom.linkTo(titleText.bottom)
-                        start.linkTo(titleText.end, 8.dp)
-                    },
-                    text = defaultDateFormatter.format(recipe.createdAt.toDate()),
-                    style = MeatInTypography.description
-                )
-                Icon(
-                    modifier = Modifier
-                        .size(18.dp)
-                        .constrainAs(shareIcon) {
-                            top.linkTo(titleText.top)
-                            bottom.linkTo(titleText.bottom)
-                            end.linkTo(parent.end)
-                        },
-                    imageVector = Icons.Default.Share, tint = Color.Black,
-                    contentDescription = "Share",
-                )
-                CoreText(
-                    modifier = Modifier.constrainAs(descriptionText) {
-                        top.linkTo(titleText.bottom, 8.dp)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        width = Dimension.fillToConstraints
-                    },
-                    text = recipe.description, style = MeatInTypography.regular,
-                )
-                ProfileButton(
-                    modifier = Modifier
-                        .constrainAs(profileButton) {
-                            top.linkTo(descriptionText.bottom, 13.dp)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                        }
-                        .fillMaxWidth(),
-                    user = recipe.author, profileUri = recipe.author.profileImage,
-                    onClick = onProfileButtonClick,
-                )
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .constrainAs(additionalInfoRow) {
-                            top.linkTo(profileButton.bottom, 22.dp)
-                        }
-                ) {
-                    VerticalLabel(title = "소요시간", content = recipe.duration.toRegularTime())
-                    VerticalLabel(title = "난이도", content = recipe.difficulty.label)
-                }
-            }
+            ThumbnailImage(recipe.thumbnail)
+            Header(recipe, onProfileButtonClick)
             SectionDivider()
-
-            BoxWithConstraints(
-                Modifier
-                    .background(Color.White)
-                    .padding(16.dp)
-                    .wrapContentSize()
-            ) {
-                val width = with(LocalDensity.current) {
-                    maxWidth.value
-                }
-                val height = width * 9 / 16
-
-                AndroidView(
-                    factory = { context ->
-                        val frameVideo =
-                            "<html><style>body {margin: 0}</style><body><iframe width=\"$width\" height=\"$height\" src=\"https://www.youtube.com/embed/${recipe.youtube}\" frameborder=\"0\" allowfullscreen></iframe></body></html>"
-
-                        WebView(context).apply {
-                            this.webViewClient = object : WebViewClient() {
-                                override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
-                                    return if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
-                                        view.context.startActivity(
-                                            Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                                        true
-                                    } else {
-                                        false
-                                    }
-                                }
-                            }
-                            this.webChromeClient = WebChromeClient()
-                            val webSettings = this.settings
-                            webSettings.javaScriptEnabled = true
-                            this.loadData(frameVideo, "text/html", "utf-8")
-                            this.minimumWidth = width.toInt()
-                            this.minimumHeight = height.toInt()
-                            this.loadUrl(frameVideo)
-                        }
-                    }
-                )
-            }
-
+            YoutubeDisplay(recipe.youtube)
             SectionDivider()
-            Column(
-                Modifier
-                    .background(Color.White)
-                    .padding(start = 16.dp, top = 16.dp, end = 16.dp)
-                    .fillMaxWidth()
-            ) {
-                CoreText(text = "재료", style = MeatInTypography.sectionHeader)
-            }
+            IngredientHeader()
         }
-        items(recipe.ingredients.windowed(2, 2, true)) { item ->
-            Row(
-                Modifier
-                    .background(Color.White)
-                    .padding(top = 10.dp, start = 16.dp, end = 16.dp, bottom = 0.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                IngredientItem(onClick = { /*TODO*/ }, ingredient = item[0], modifier = Modifier.weight(1f))
-                IngredientItem(onClick = { /*TODO*/ }, ingredient = item[1], modifier = Modifier.weight(1f))
-            }
-        }
+        listIngredients(recipe.ingredients)
         item {
             Spacer(
                 Modifier
@@ -211,69 +76,247 @@ fun RecipeOverviewScreen(
                     .fillParentMaxWidth()
             )
             SectionDivider()
+            RecipeSteps(recipe.briefContent)
+            SectionDivider()
+            PostHeader()
         }
-        item {
-            ConstraintLayout(
-                modifier = Modifier
-                    .background(Color.White)
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                val (headerText, recipeList) = createRefs()
+        listPosts(recipe.linkedPosts)
+    }
+}
 
-                CoreText(
-                    text = "레시피 브리핑", style = MeatInTypography.sectionHeader,
-                    modifier = Modifier.constrainAs(headerText) {
-                        start.linkTo(parent.start)
-                        top.linkTo(parent.top)
-                    }
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+private fun ThumbnailImage(thumbnailUri: String) {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp),
+        painter = rememberImagePainter(data = thumbnailUri),
+        contentDescription = "Recipe overview image",
+        contentScale = ContentScale.Crop,
+    )
+
+}
+
+private fun LazyListScope.listPosts(linkedPosts: List<BriefPost>) {
+    items(linkedPosts) { post ->
+        PostCard(
+            post = post, onClick = { /* TODO */ },
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxWidth()
+        )
+        SectionDivider()
+    }
+
+}
+
+@Composable
+private fun PostHeader() {
+    CoreText(
+        modifier = Modifier
+            .background(Color.White)
+            .padding(top = 16.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
+            .fillMaxWidth(),
+        text = "연결된 포스트", style = MeatInTypography.sectionHeader
+    )
+}
+
+@Composable
+private fun RecipeSteps(recipeSteps: List<String>) {
+    ConstraintLayout(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        val (headerText, recipeList) = createRefs()
+
+        CoreText(
+            text = "레시피 브리핑", style = MeatInTypography.sectionHeader,
+            modifier = Modifier.constrainAs(headerText) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+            }
+        )
+
+        Column(
+            modifier = Modifier.constrainAs(recipeList) {
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(headerText.bottom, 10.dp)
+            },
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            recipeSteps.forEachIndexed { index, description ->
+                RecipeBriefItem(
+                    index = index + 1, description = description,
+                    modifier = Modifier.fillMaxWidth()
                 )
-
-                Column(
-                    modifier = Modifier.constrainAs(recipeList) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(headerText.bottom, 10.dp)
-                    },
-                    verticalArrangement = Arrangement.spacedBy(10.dp)
-                ) {
-                    recipe.briefContent.forEachIndexed { index, description ->
-                        RecipeBriefItem(
-                            index = index + 1, description = description,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
             }
         }
-        item {
-            SectionDivider()
-            CoreText(
-                modifier = Modifier
-                    .background(Color.White)
-                    .padding(top = 16.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
-                    .fillMaxWidth(),
-                text = "연결된 포스트", style = MeatInTypography.sectionHeader
-            )
-        }
-        items(recipe.linkedPosts) { post ->
-            PostCard(
-                post = post, onClick = { /* TODO */ },
-                modifier = Modifier
-                    .background(Color.White)
-                    .fillMaxWidth()
-            )
-            SectionDivider()
+    }
+}
+
+private fun LazyListScope.listIngredients(ingredients: List<Ingredient>) {
+    items(ingredients.windowed(2, 2, true)) { item ->
+        Row(
+            Modifier
+                .background(Color.White)
+                .padding(top = 10.dp, start = 16.dp, end = 16.dp, bottom = 0.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            IngredientItem(onClick = { /*TODO*/ }, ingredient = item[0], modifier = Modifier.weight(1f))
+            IngredientItem(onClick = { /*TODO*/ }, ingredient = item[1], modifier = Modifier.weight(1f))
         }
     }
 }
 
 @Composable
-fun SectionDivider() {
+private fun IngredientHeader() {
+    Column(
+        Modifier
+            .background(Color.White)
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+    ) {
+        CoreText(text = "재료", style = MeatInTypography.sectionHeader)
+    }
+}
+
+@Composable
+private fun YoutubeDisplay(youtubeUri: String) {
+    BoxWithConstraints(
+        Modifier
+            .background(Color.White)
+            .padding(16.dp)
+            .wrapContentHeight()
+            .fillMaxWidth()
+    ) {
+        if (!youtubeUri.matches("""^[0-9A-Za-z_-]{11}$""".toRegex())) {
+            Text(text = "유튜브 링크가 손상되어 영상을 표시할 수 없습니다.")
+            return@BoxWithConstraints
+        }
+
+        val width = with(LocalDensity.current) {
+            maxWidth.value
+        }
+        val height = width * 9 / 16
+
+        AndroidView(
+            factory = { context ->
+                val frameVideo = "<html><style>body {margin: 0}</style><body><iframe width=\"$width\" " +
+                        "height=\"$height\" src=\"https://www.youtube.com/embed/$youtubeUri\" frameborder=\"0\" " +
+                        "allowfullscreen></iframe></body></html>"
+
+                WebView(context).apply {
+                    this.webViewClient = object : WebViewClient() {
+                        override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+                            return if (url != null && url.startsWith("https://")) {
+                                view.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                    }
+                    this.webChromeClient = WebChromeClient()
+                    val webSettings = this.settings
+                    webSettings.javaScriptEnabled = true
+                    this.loadData(frameVideo, "text/html", "utf-8")
+                    this.minimumWidth = width.toInt()
+                    this.minimumHeight = height.toInt()
+                    this.loadUrl(frameVideo)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun Header(recipe: Recipe, onProfileButtonClick: () -> Unit) {
+    ConstraintLayout(
+        Modifier
+            .background(Color.White)
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        val (
+            titleText,
+            dateText,
+            shareIcon,
+            descriptionText,
+            profileButton,
+            additionalInfoRow,
+        ) = createRefs()
+        CoreText(
+            modifier = Modifier.constrainAs(titleText) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            },
+            text = recipe.name, style = MeatInTypography.sectionHeader
+        )
+        CoreText(
+            modifier = Modifier.constrainAs(dateText) {
+                bottom.linkTo(titleText.bottom)
+                start.linkTo(titleText.end, 8.dp)
+            },
+            text = defaultDateFormatter.format(recipe.createdAt.toDate()),
+            style = MeatInTypography.description
+        )
+        Icon(
+            modifier = Modifier
+                .size(18.dp)
+                .constrainAs(shareIcon) {
+                    top.linkTo(titleText.top)
+                    bottom.linkTo(titleText.bottom)
+                    end.linkTo(parent.end)
+                },
+            imageVector = Icons.Default.Share, tint = Color.Black,
+            contentDescription = "Share",
+        )
+        CoreText(
+            modifier = Modifier.constrainAs(descriptionText) {
+                top.linkTo(titleText.bottom, 8.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                width = Dimension.fillToConstraints
+            },
+            text = recipe.description, style = MeatInTypography.regular,
+        )
+        ProfileButton(
+            modifier = Modifier
+                .shadow(1.dp, RoundedCornerShape(5.dp))
+                .constrainAs(profileButton) {
+                    top.linkTo(descriptionText.bottom, 13.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .fillMaxWidth(),
+            user = recipe.author, profileUri = recipe.author.profileImage,
+            onClick = onProfileButtonClick,
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(additionalInfoRow) {
+                    top.linkTo(profileButton.bottom, 22.dp)
+                }
+        ) {
+            VerticalLabel(title = "소요시간", content = recipe.duration.formatAsTime())
+            VerticalLabel(title = "난이도", content = recipe.difficulty.label)
+        }
+    }
+}
+
+@Composable
+private fun SectionDivider() {
     Spacer(modifier = Modifier.height(8.dp))
 }
 
-private fun Int.toRegularTime(): String {
+private fun Int.formatAsTime(): String {
     val hours = this / 3600
     val minutes = this / 60 % 60
     val seconds = this % 60
@@ -294,7 +337,7 @@ private fun VerticalLabel(title: String, content: String) {
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xffffffff)
+@Preview(showBackground = true, backgroundColor = 0xffffffff, heightDp = 1300)
 @Composable
 fun RecipeOverviewScreenPreview() {
     RecipeOverviewScreen(recipe = FakeValues.RECIPE, onProfileButtonClick = {})
