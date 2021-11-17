56f5292e2a7ecca544c54a17328fadbaafcb044a
Beomjun Kim <nutyworks@gmail.com>
2021-11-17 20:42:27 +0900

    fix(presentation): fix `PostPreviewCard` composable

--- app/src/main/java/app/meatin/ui/composables/components/PostPreviewCard.kt
+++ app/src/main/java/app/meatin/ui/composables/components/PostPreviewCard.kt
@@ -1,39 +1,39 @@
 package app.meatin.ui.composables.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.meatin.R
+import app.meatin.domain.model.BriefCommunityUser
+import app.meatin.domain.model.BriefPost
 import app.meatin.domain.model.FakeValues
-import app.meatin.domain.model.Post
-import app.meatin.ui.theme.DarkFlamingo
+import app.meatin.domain.model.Heart
+import app.meatin.ui.theme.BoxTextDarkGray
+import app.meatin.ui.theme.Flamingo
 import app.meatin.ui.theme.MeatInTypography
 import app.meatin.ui.theme.composefix.CoreText
 import app.meatin.util.defaultDateFormatter
@@ -41,117 +41,91 @@ import app.meatin.util.toDate
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 
-@ExperimentalCoilApi
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun PostPreviewCard(
     modifier: Modifier = Modifier,
-    post: Post,
-    onClick: () -> Unit,
+    post: BriefPost,
 ) {
-    Card(
-        modifier
-            .requiredHeight(302.dp)
-            .requiredWidth(375.dp)
-            .clip(RoundedCornerShape(5.dp))
-            .clickable { onClick() },
-        backgroundColor = Color.White,
+    Column(
+        modifier.then(
+            Modifier.background(Color.White)
+        ),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .padding(12.dp)
-        ) {
-            val dateStr: String = defaultDateFormatter.format(post.createdAt.toDate())
-            Column(Modifier.align(Alignment.TopStart)) {
-                Row {
-                    Image(
-                        modifier = Modifier
-                            .height(40.dp)
-                            .width(40.dp)
-                            .clip(CircleShape),
-                        painter = rememberImagePainter(
-                            data = post.author.profileImage
-                        ),
-                        contentDescription = null
-                    )
-                    Column {
-                        BadgedUser(user = post.author)
-                        Spacer(modifier = Modifier.height(4.dp))
-                        CoreText(
-                            text = dateStr,
-                            overflow = TextOverflow.Ellipsis,
-                            style = MeatInTypography.regular,
-                            maxLines = 1,
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                CoreText(
-                    text = post.content,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MeatInTypography.regular,
-                    maxLines = 1,
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                Image(
-                    modifier = Modifier
-                        .height(170.dp)
-                        .width(343.dp),
-                    painter = rememberImagePainter(
-                        data = post.photos.first()
-                    ),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    val heartIcon = if (post.heart.hearted) {
-                        Icons.Default.Favorite
-                    } else {
-                        Icons.Default.FavoriteBorder
-                    }
-                    Icon(
-                        modifier = Modifier
-                            .padding(end = 2.dp)
-                            .size(12.dp),
-                        imageVector = heartIcon,
-                        contentDescription = null,
-                        tint = DarkFlamingo,
-                    )
-                    CoreText(
-                        text = post.heart.count.toString(),
-                        style = MeatInTypography.regularImportant
-                            .copy(fontSize = 12.sp),
-                        color = DarkFlamingo,
-                    )
-                    Spacer(modifier = Modifier.width(11.dp))
-                    Icon(
-                        modifier = Modifier
-                            .padding(end = 2.dp)
-                            .size(12.dp),
-                        imageVector = Icons.Default.Send,
-                        contentDescription = null,
-                        tint = Color.Gray,
-                    )
-                    CoreText(
-                        text = post.comments.size.toString(),
-                        style = MeatInTypography.regularImportant
-                            .copy(fontSize = 12.sp),
-                        color = Color.Gray,
-                    )
-                }
-            }
-        }
+        PostHeader(post.author, post.createdAt)
+        CoreText(post.content)
+        Image(
+            contentScale = ContentScale.FillWidth,
+            painter = rememberImagePainter(data = post.photo), contentDescription = "Preview image"
+        )
+        ExtraInfo(post.heart, post.nComments)
     }
 }
 
+@Composable
+private fun ExtraInfo(heart: Heart, nComments: Int) =
+    Row(horizontalArrangement = Arrangement.spacedBy(12.dp), verticalAlignment = Alignment.CenterVertically) {
+        val heartIcon = if (heart.hearted) {
+            painterResource(id = R.drawable.ic_heart2_filled)
+        } else {
+            painterResource(id = R.drawable.ic_heart2)
+        }
+
+        IconText(
+            painter = heartIcon, iconSize = 12.dp,
+            text = heart.count.toString(), fontSize = 12.sp,
+            color = Flamingo,
+            contentDescription = if (heart.hearted) "Hearted" else "Not hearted",
+        )
+        IconText(
+            painter = painterResource(id = R.drawable.ic_comment), iconSize = 12.dp,
+            text = nComments.toString(), fontSize = 12.sp,
+            color = BoxTextDarkGray,
+            contentDescription = "Comments",
+        )
+    }
+
+@Composable
+fun IconText(
+    painter: Painter, iconSize: Dp,
+    text: String, fontSize: TextUnit,
+    color: Color,
+    contentDescription: String,
+) = Row(horizontalArrangement = Arrangement.spacedBy(4.dp), verticalAlignment = Alignment.CenterVertically) {
+    Icon(
+        modifier = Modifier.size(iconSize),
+        painter = painter, contentDescription = contentDescription,
+        tint = color,
+    )
+    Text(text, color = color, fontSize = fontSize)
+}
+
 @ExperimentalCoilApi
+@Composable
+private fun PostHeader(user: BriefCommunityUser, date: Long) =
+    Row(horizontalArrangement = Arrangement.spacedBy(12.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            painter = rememberImagePainter(data = user.profileImage), contentDescription = "User profile image",
+        )
+        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            BadgedUser(user = user)
+            CoreText(defaultDateFormatter.format(date.toDate()), style = MeatInTypography.description)
+        }
+    }
+
 @Preview
 @Composable
-fun PostCardPreview() {
+fun PPCPreview() = Surface(Modifier.background(Color.Gray)) {
     PostPreviewCard(
-        modifier = Modifier.padding(10.dp),
-        post = FakeValues.POST,
-        onClick = { },
+        modifier = Modifier
+            .width(343.dp)
+            .wrapContentHeight()
+            .padding(10.dp)
+            .clickable { },
+        post = FakeValues.BRIEF_POST,
     )
 }
