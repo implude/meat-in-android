8ff8fa429ce3f59a96e6a1815a04b7416fb43eb4
gijuno <itx.ljh.developer@gmail.com>
2021-12-01 20:19:05 +0900

    feat: change ui and texts of chip

--- app/src/main/java/app/meatin/ui/composables/components/Chip.kt
+++ app/src/main/java/app/meatin/ui/composables/components/Chip.kt
@@ -2,44 +2,28 @@ package app.meatin.ui.composables.components
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import app.meatin.domain.model.Comment
-import app.meatin.domain.model.FakeValues
+import app.meatin.ui.theme.BorderGray
+import app.meatin.ui.theme.DisableLightGray2
+import app.meatin.ui.theme.Flamingo
 import app.meatin.ui.theme.MeatInTypography
 import app.meatin.ui.theme.composefix.CoreText
-import app.meatin.util.defaultDateFormatter
-import app.meatin.util.toDate
 import coil.annotation.ExperimentalCoilApi
 
 @SuppressLint("UnrememberedMutableState")
@@ -47,13 +31,13 @@ import coil.annotation.ExperimentalCoilApi
 @Preview(showBackground = true)
 @Composable
 fun View() {
-    var selectedCar: Car? by remember { mutableStateOf(Car.values()[0]) }
+    var selectedMeatType: MeatType? by remember { mutableStateOf(MeatType.values()[0]) }
     ChipGroup(
-        cars = getAllCars(),
-        selectedCar = selectedCar,
+        value = getAllMeatTypes(),
+        selectedValue = selectedMeatType,
         onSelectedChanged = {
             Log.d("select", it)
-            selectedCar = getCar(it)
+            selectedMeatType = getMeatType(it)
         }
     )
 }
@@ -66,10 +50,9 @@ fun Chip(
     onSelectionChanged: (String) -> Unit = {},
 ) {
     Surface(
-        modifier = Modifier.padding(4.dp),
-        elevation = 8.dp,
+        modifier = Modifier.padding(6.dp),
         shape = MaterialTheme.shapes.medium,
-        color = if (isSelected) Color.LightGray else MaterialTheme.colors.primary
+        color = if (isSelected) Flamingo else DisableLightGray2
     ) {
         Row(modifier = Modifier
             .toggleable(
@@ -81,9 +64,9 @@ fun Chip(
         ) {
             CoreText(
                 text = name,
-                style = MaterialTheme.typography.body2,
-                color = Color.White,
-                modifier = Modifier.padding(8.dp)
+                style = MeatInTypography.regular,
+                color = if (isSelected) Color.White else BorderGray,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
             )
         }
     }
@@ -93,16 +76,16 @@ fun Chip(
 @Preview(showBackground = true)
 @Composable
 fun ChipGroup(
-    cars: List<Car> = getAllCars(),
-    selectedCar: Car? = null,
+    value: List<MeatType> = getAllMeatTypes(),
+    selectedValue: MeatType? = null,
     onSelectedChanged: (String) -> Unit = {},
 ) {
     Column(modifier = Modifier.padding(8.dp)) {
         LazyRow {
-            items(cars) {
+            items(value) {
                 Chip(
                     name = it.value,
-                    isSelected = selectedCar == it,
+                    isSelected = selectedValue == it,
                     onSelectionChanged = { it1 ->
                         onSelectedChanged(it1)
                     },
@@ -112,17 +95,26 @@ fun ChipGroup(
     }
 }
 
-enum class Car(val value: String){
-    AUDI("Audi"),
-    VW("VW"),
-    BMW("BWM"),
+enum class MeatType(val value: String){
+    Pork("돼지고기"),
+    Beef("소고기"),
+    Chicken("닭고기"),
+    Processed("가공육류")
 }
 
-fun getAllCars(): List<Car>{
-    return listOf(Car.AUDI, Car.VW, Car.BMW)
+enum class Env(val value: String){
+    Pan("후라이펜"),
+    Direct("직화"),
+    Charcoal("숯불"),
+    AirFri("에어프라이기")
 }
 
-fun getCar(value: String): Car? {
-    val map = Car.values().associateBy(Car::value)
+fun getAllMeatTypes(): List<MeatType>{
+    return listOf(MeatType.Pork, MeatType.Beef, MeatType.Chicken, MeatType.Processed)
+}
+
+fun getMeatType(value: String): MeatType? {
+    val map = MeatType.values().associateBy(MeatType::value)
     return map[value]
 }
+
